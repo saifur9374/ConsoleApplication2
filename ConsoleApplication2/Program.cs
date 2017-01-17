1b88e8afe100496b3fcaf1597377d3b48184271b
saifur9374 <sarahma@microsoft.com>
2017-01-17 09:46:57 -0800

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,13 +11,12 @@ namespace ConsoAleApplication1
         static void Main(string[] args)
         {
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open("c:\\Users\sarahma\Documents\Disney\Disney%20Destinations\HoneyComb\WHOLESALE%20-%20WDW%20-%20RAW%20DATA\WHOLESALE%20-%20WDW%20-%20RAW%20DATA/FY17\Wholesale%20Data_ADM_RN.xlsx"
-");
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open("c:\\adfgetstarted\\testinput.xlsx");
             xlApp.Visible = true;
             foreach (Excel.Worksheet sht in xlWorkBook.Worksheets)
             {
                 sht.Select();
-                xlWorkBook.SaveAs(string.Format("{0}{1}.csv", "c:\\adfgetstarted\test", sht.Name), Excel.XlFileFormat.xlCSV, Excel.XlSaveAsAccessMode.xlNoChange);
+                xlWorkBook.SaveAs(string.Format("{0}{1}.csv", "c:\\adfgetstarted\\testoutput", sht.Name), Excel.XlFileFormat.xlCSV, Excel.XlSaveAsAccessMode.xlNoChange);
 
             }
             xlWorkBook.Close(false);
